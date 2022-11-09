53032c1670795236118078973f0f16489d4cc677
Ichigo <77748709+weirdlysimple@users.noreply.github.com>
2022-11-09 13:08:35 +0000

    add comments to flashlight.cpp

--- flashlight.cpp
+++ flashlight.cpp
@@ -1,14 +1,14 @@
 struct button_ripple_t {
-    ImVec2 position;
-    float time;
-    float eased;
+    ImVec2 position; // position of where we clicked
+    float time; // life time
+    float eased; // other life time we use to ease our lifetime (pretty stupidly)
 };
 
 void menu::frame() {
 
     static ImVec2 position = { 125, 125 };
 
-    auto blend_color = [&](ImColor first, ImColor second, float additive) -> ImColor {
+    const auto blend_color = [&](ImColor first, ImColor second, float additive) -> ImColor {
 
         const auto r = std::lerp(first.Value.x, second.Value.x, additive);
         const auto g = std::lerp(first.Value.y, second.Value.y, additive);
@@ -19,31 +19,37 @@ void menu::frame() {
 
     static std::vector<button_ripple_t> ripples;
 
-    auto circ_in = [&](float v) -> float {
+    // https://easings.net/#easeInQuad
+    const auto quad_ease_in = [&](float v) -> float {
         return (v * v);
     };
 
-    auto button = [&](ImVec2 position, ImVec2 size, const std::string& label, const ImColor& bg_color, const ImColor& text_color) -> bool {
+    const auto button = [&](ImVec2 position, ImVec2 size, const std::string& label, const ImColor& bg_color, const ImColor& text_color) -> bool {
 
-        auto hovering = ImGui::IsMouseHoveringRect(position, { position.x + size.x, position.y + size.y }, false);
-        auto clicked = hovering && ImGui::IsMouseClicked(ImGuiMouseButton_Left);
+        const auto hovering = ImGui::IsMouseHoveringRect(position, { position.x + size.x, position.y + size.y }, false);
+        const auto clicked = hovering && ImGui::IsMouseClicked(ImGuiMouseButton_Left);
 
         ImGui::PushFont(menu::small_font);
 
         const auto text_size = ImGui::CalcTextSize(label.data());
         ImGui::GetBackgroundDrawList()->AddRectFilled(position, { position.x + size.x, position.y + size.y }, bg_color, 3.f);
-        ImGui::GetBackgroundDrawList()->PushClipRect(position, { position.x + size.x, position.y + size.y });
 
+        // push a clipped rect to keep our circles inside of the button
+        ImGui::GetBackgroundDrawList()->PushClipRect(position, { position.x + size.x, position.y + size.y });
+        // check if our ripple vector actually has a ripple
         if (!ripples.empty()) {
+            // iterate through each ripple and animate the circle with our circ_in function above :D
             auto it = ripples.begin();
             while (it != ripples.end()) {
 
+                // 700ms life time
                 it->eased -= (1.f / 0.7f) * ImGui::GetIO().DeltaTime;
-                it->time = circ_in(it->eased);
+                it->time = quad_ease_in(it->eased);
 
                 ImGui::GetBackgroundDrawList()->AddCircleFilled(it->position, 180 * (1.f - it->time), ImColor(text_color.Value.x, text_color.Value.y, text_color.Value.z, it->time * 0.8f));
                 ImGui::GetBackgroundDrawList()->AddCircle(it->position, 180 * (1.f - it->time), ImColor(text_color.Value.x, text_color.Value.y, text_color.Value.z, it->time));
 
+                // once you can't see the ripple, remove it.
                 if (it->eased <= 0.f) {
                     it = ripples.erase(it);
                 }
@@ -58,6 +64,7 @@ void menu::frame() {
 
         ImGui::PopFont();
 
+        // if we clicked our button we want to push back where we clicked and also return true.
         if (clicked) {
             ripples.push_back({ ImGui::GetIO().MousePos, 1.f, 1.f });
 
@@ -65,16 +72,11 @@ void menu::frame() {
         }
     };
 
-    auto begin_frame = [&](ImVec2 position, bool is_open) -> bool {
-        if (!is_open) {
-            return false;
-        }
-
+    const auto begin_frame = [&](ImVec2 position) -> bool {
         static bool needs_power = true;
-        if (GetAsyncKeyState(VK_END) & 1) needs_power = !needs_power;
-
         static float power = 0.f;
 
+        // on and off magic https://en.wikipedia.org/wiki/Linear_interpolation
         if (power != needs_power) {
             power += (needs_power - power) * 0.1f;
         }
@@ -85,9 +87,10 @@ void menu::frame() {
         const auto text_size = ImGui::CalcTextSize("Flashlight");
 
         ImGui::GetBackgroundDrawList()->AddRectFilled(position, { position.x + size.x, position.y + size.y }, bg_color, 3.0f);
-        ImGui::GetBackgroundDrawList()->AddRect({position.x - 1, position.y - 1}, {position.x + size.x + 1, position.y + size.y + 1}, text_color, 3.0f);
+        ImGui::GetBackgroundDrawList()->AddRect({ position.x - 1, position.y - 1 }, { position.x + size.x + 1, position.y + size.y + 1 }, text_color, 3.0f);
         ImGui::GetBackgroundDrawList()->AddText(ImVec2(position.x + size.x / 2 - text_size.x / 2, position.y + 15), text_color, "Flashlight");
 
+        // our toggle button
         if (button({ position.x + 15, position.y + size.y - 15 - 40 }, { size.x - 30, 40 }, "Toggle flashlight", text_color, bg_color)) {
             needs_power = !needs_power;
         }
@@ -95,8 +98,6 @@ void menu::frame() {
         return true;
     };
 
-
-    if (begin_frame(position, true)) {
-
-    }
+    // render everything
+    begin_frame(position);
 }
